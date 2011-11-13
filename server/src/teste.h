01dc9ceacfaa69f5213fa785421de74da77360cc
Marcelo Elias Del Valle <mvallebr@gmail.com>
2011-11-13 01:12:41 -0200

    teste

---
+++ server/src/teste.h
@@ -0,0 +1,17 @@
+/*
+ * teste.h
+ *
+ *  Created on: 13/11/2011
+ *      Author: mvalle
+ */
+
+#ifndef TESTE_H_
+#define TESTE_H_
+
+class teste {
+public:
+	teste();
+	virtual ~teste();
+};
+
+#endif /* TESTE_H_ */
